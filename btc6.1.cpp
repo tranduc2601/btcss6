4968797afb745b39e6983111049b80b17b29fc65
tranduc2601 <breakgamer2601@gmail.com>
2024-12-24 22:00:31 +0700

    Add files via upload

---
+++ btc6.1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int n ,sum=0;
+	printf(" nhap vao 5 so nguyen: \n");
+	for(int i = 1; i <= 5; i++ ){
+		printf(" so thu %d ",i);
+		scanf("%d ", &n );
+		printf() 
+		if (n % 2 != 0){
+			sum  +=n; 
+		}
+	}
+	printf(" tong cac so lr da nhap la: ",sum);
+	return 0;
+}
